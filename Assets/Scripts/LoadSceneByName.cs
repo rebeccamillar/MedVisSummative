ba2308211d96b311699e92e128c5783a2784c5c7
rebeccamillar <rm321@live.co.uk>
2024-11-29 11:54:27 +0000

    Button Sounds
    yippee

--- Assets/Scripts/LoadSceneByName.cs
+++ Assets/Scripts/LoadSceneByName.cs
@@ -7,7 +7,14 @@ public class LoadSceneByName : MonoBehaviour
 {
     public void LoadSceneBtn(string sceneName)
     {
+        StartCoroutine(DelayToLoadScene(sceneName));
+    }
+
+    private IEnumerator DelayToLoadScene(string sceneName)
+    {
+        yield return new WaitForSeconds(0.5f);
         SceneManager.LoadScene(sceneName);
+
     }
 
 }
