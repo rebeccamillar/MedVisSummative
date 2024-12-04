9019f37d896a2c430e2e56f6f9cc36be459b9d25
Eilidh Stevenson <eilidhstevenson@gmail.com>
2024-12-04 09:52:25 +0000

    fixed quiz error

--- Assets/Scripts/ManageQuiz.cs
+++ Assets/Scripts/ManageQuiz.cs
@@ -12,38 +12,40 @@ public class ManageQuiz : MonoBehaviour
     private GameObject scorePanel;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        OnClickNext();
-    }
+    //void Start()
+    //{
+        //OnClickNext();
+    //}
 
     // Update is called once per frame
-    void Update()
-    {
+    //void Update()
+    //{
         
-    }
+    //}
 
-    public void OnClickNext()
-    {  
-        if(myQuestionIndex < maxQuestionIndex)
-        {
-        //retrieve random question from list 
-        int index = Random.Range(0,questionList.Count);
-        //set question to visible (set GO as Active)
-        questionList[index].SetActive(true);
-        //Remove object (question) from List
-        questionList.RemoveAt(index);
-        //increase myQuestion Index value
-        myQuestionIndexindex +=1;
-        }
-
-        else
-        {
+    //public void OnClickNext()
+    //{  
+        //if(myQuestionIndex < maxQuestionIndex)
+        //{
+            //retrieve random question from list 
+            //int index = Random.Range(0,questionList.Count);
+            //set question to visible (set GO as Active)
+            //questionList[index].SetActive(true);
+            //Remove object (question) from List
+            //questionList.RemoveAt(index);
+            //increase myQuestion Index value
+            //myQuestionIndexindex +=1;
+        //}
+//}
+        //else
+        //{
             //Show score panel
-            scorePanel.SetActive(true);
-            Debug.Log("End")
-        }
+           // scorePanel.SetActive(true);
+           // Debug.Log("End");
+       // }
 
-    }
+
     //*for(int i = 0; i < questionList.transform.childCount; i++){questionList.transform.GetChild(i).gameObject.SetActive(false);}//*
+
 }
+
